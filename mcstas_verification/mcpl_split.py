4cefb6af11374a3abd983da7f71b50baa315daf0
Diablo <daniel.lomholt.2000@gmail.com>
2026-05-02 16:53:39 +0200

    Add mcpl split and mcstas run to do benchmark of generated data

---
+++ mcstas_verification/mcpl_split.py
@@ -0,0 +1,38 @@
+import sys
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+sys.path.append("..")
+from data_load import load_mcpl_file, save_data_as_mcpl
+import numpy as np
+import copy
+
+def split_mcpl_file(mcpl_file, requested_sizes, filename, input_particles):
+    original_data = load_mcpl_file("../ODIN.mcpl.gz", n_particles=1271093).numpy()
+    data = load_mcpl_file(mcpl_file, n_particles=input_particles).numpy()
+
+    print(data.shape)
+    total_I = original_data[:,0].sum()
+    print(total_I)
+
+    for size in requested_sizes:
+        
+        size = int(size)
+        idx = np.random.randint(0,data.shape[0], size)
+        new_data = copy.copy(data)
+        diff = total_I - data[idx,0].sum()
+
+        new_data[idx, 0] += diff/len(idx)
+        print(f"size={size}\ntotal I of new data = {new_data[idx,0].sum()}")
+        save_data_as_mcpl(new_data[idx], filename=filename + f"_{size}")
+
+
+
+if __name__ == "__main__":
+    requested_sizes = np.logspace(1, 6, 6)
+    split_mcpl_file("../ODIN.mcpl.gz", requested_sizes, "./mcpl_files/input", 1000000)
+    print("\n")
+    split_mcpl_file("../cmf_samples.mcpl.gz", requested_sizes, "./mcpl_files/cfm", 1000000)
+
+
+
+
---
+++ mcstas_verification/mcstas_runs.py
@@ -0,0 +1,28 @@
+import mcstasscript as ms
+import os
+import copy
+
+instr = ms.McStas_instr(
+    "odin_sample", author="Daniel Lomholt Christensen", origin="UCPH @ NBI"
+)
+
+
+mcstas_file = ms.McStas_file("./odin_sample.instr")
+
+mcstas_file.add_to_instr(instr)
+
+instr.set_parameters(run_from_mcpl=f'"../ODIN.mcpl.gz"')
+instr.settings(output_path="simulations/raw") 
+instr.backengine()
+
+for file in os.listdir("mcpl_files"):
+    if not file.endswith(".mcpl.gz"):
+        continue
+    instr.set_parameters(run_from_mcpl=f'"mcpl_files/{file}"')
+    name = copy.copy(file)
+    name = name.strip(".mcpl.gz")
+    instr.settings(output_path="simulations/" + name) 
+    instr.backengine()
+    instr.settings(force_compile=False)
+
+
