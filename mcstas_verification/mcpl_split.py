75475205b02b7e12ace3bff01a209c5399a1eea9
Diablo <daniel.lomholt.2000@gmail.com>
2026-05-05 10:54:15 +0200

    Small changes in mcpl split to use the big file with 254M neutrons

--- mcstas_verification/mcpl_split.py
+++ mcstas_verification/mcpl_split.py
@@ -6,32 +6,31 @@ from data_load import load_mcpl_file, save_data_as_mcpl
 import numpy as np
 import copy
 
-def split_mcpl_file(mcpl_file, requested_sizes, filename, input_particles):
-    original_data = load_mcpl_file("../ODIN.mcpl.gz", n_particles=1271093).numpy()
+def split_mcpl_file(mcpl_file, requested_sizes, filename, input_particles, original_data):
     data = load_mcpl_file(mcpl_file, n_particles=input_particles).numpy()
 
     print(data.shape)
     total_I = original_data[:,0].sum()
-    print(total_I)
+    print(f"Original data Intensity = {total_I}")
 
     for size in requested_sizes:
-        
         size = int(size)
         idx = np.random.randint(0,data.shape[0], size)
         new_data = copy.copy(data)
         diff = total_I - data[idx,0].sum()
 
-        new_data[idx, 0] += diff/len(idx)
+        new_data[idx, 0] *= diff/len(idx)
         print(f"size={size}\ntotal I of new data = {new_data[idx,0].sum()}")
         save_data_as_mcpl(new_data[idx], filename=filename + f"_{size}")
 
 
 
 if __name__ == "__main__":
+    original_data = load_mcpl_file("../ODIN.mcpl.gz", n_particles=254_134_779).numpy()
     requested_sizes = np.logspace(1, 6, 6)
-    split_mcpl_file("../ODIN.mcpl.gz", requested_sizes, "./mcpl_files/input", 1000000)
+    split_mcpl_file("../ODIN.mcpl.gz", requested_sizes, "./mcpl_files/input", 1_000_000, original_data)
     print("\n")
-    split_mcpl_file("../cmf_samples.mcpl.gz", requested_sizes, "./mcpl_files/cfm", 1000000)
+    split_mcpl_file("../cmf_samples.mcpl.gz", requested_sizes, "./mcpl_files/cfm", 10_000_000, original_data)
 
 
 
