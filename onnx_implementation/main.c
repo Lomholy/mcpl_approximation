fb173c3972b19cd284fd1054078d20e6cf1be8c8
Diablo <daniel.lomholt.2000@gmail.com>
2026-06-03 11:52:55 +0200

    Add working prototype using onnxruntime

--- onnx_implementation/main.c
+++ onnx_implementation/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include "../../../../../onnxruntime-osx-arm64-1.26.0/include/onnxruntime_c_api.h"
+// #include "../../../../../onnxruntime-osx-arm64-1.26.0/include/onnxruntime_c_api.h"
+#include "onnxruntime/core/session/onnxruntime_c_api.h"
+// #include "../../../../../onnxruntime-osx-arm64-1.26.0/include/coreml_provider_factory.h"
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
@@ -15,7 +17,7 @@
 
 
 void save_to_csv(const char* filename, float* data, int n_samples, int dim) {
-    FILE* file = fopen(filename, "w");
+    FILE* file = fopen(filename, "a");
     if (!file) {
         perror("Error opening file");
         return;
@@ -35,62 +37,77 @@ void save_to_csv(const char* filename, float* data, int n_samples, int dim) {
     fclose(file);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  
+    // ========================================================================
+    // ============================ LOAD INPUT ARGS ===========================
+    // ========================================================================
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <n_samples>\n", argv[0]);
+        return 1;
+    }
+    
+    int n_samples = atoi(argv[1]);
+    if (n_samples <= 0) {
+        fprintf(stderr, "n_samples must be positive\n");
+        return 1;
+    }
+    
+    // ========================================================================
+    // ======================= START ORT SESSION ==============================
+    // ========================================================================
+
     const OrtApi* api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
 
     OrtEnv* env;
-    OrtStatus* status;
 
     // Create environment
-    status = api->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "test", &env);
-    if (status != NULL) {
-        fprintf(stderr, "Error creating environment: %s\n", api->GetErrorMessage(status));
-        api->ReleaseStatus(status);
-        return 1;
-    }
+    CHECK(api->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "test", &env));
 
     // Create session options
     OrtSessionOptions* session_options;
-    status = api->CreateSessionOptions(&session_options);
-    
-    if (status != NULL) {
-        fprintf(stderr, "Error creating session options: %s\n", api->GetErrorMessage(status));
-        return 1;
-    }
+    CHECK(api->CreateSessionOptions(&session_options));
+    // CHECK(api->SetSessionLogSeverityLevel(session_options, 0));
+    // Optional: set number of threads. 0 means auto
+    CHECK(api->SetIntraOpNumThreads(session_options, 0));
+    CHECK(api->SetInterOpNumThreads(session_options, 0));
 
-    // Optional: set number of threads
-    CHECK(api->SetIntraOpNumThreads(session_options, 1));
+    // Set graph optimizations on
+    CHECK(api->SetSessionGraphOptimizationLevel(
+      session_options, ORT_ENABLE_ALL));
 
-    // Load model
-    const char* model_path = "./velocity_field.onnx";
-
+    // ========================================================================
+    // =========================== LOAD MODEL =================================
+    // ========================================================================
+    const char* model_path = "/Users/tqv636/Phd/Projects/machine_learning/mcpl_approximation/onnx_implementation/velocity_field.onnx";
     OrtSession* session;
-    status = api->CreateSession(env, model_path, session_options, &session);
-    if (status != NULL) {
-        fprintf(stderr, "Error loading model: %s\n", api->GetErrorMessage(status));
-        api->ReleaseStatus(status);
-        return 1;
-    }
+    CHECK(api->CreateSession(env, model_path, session_options, &session));
+    printf("Model loaded successfully!\n");
+    // ========================================================================
+    // =========================== PREPARE SAMPLING ===========================
+    // ========================================================================
+    // Delete old samples
+    const char *sample_file = "C_samples.csv";
 
-    printf("✅ Model loaded successfully!\n");
-    // ---------------------------
-    // Sampling parameters
-    // ---------------------------
-    int n_samples = 10000;
+    if (remove(sample_file) == 0) {
+        printf("File deleted successfully.\n");
+    } else {
+        printf("Error: Unable to delete the file.\n");
+    }
+ 
+
+    int batch_size = 10000; // tune this
+    if (batch_size > n_samples) batch_size = n_samples;
+    int n_batches = (n_samples + batch_size - 1) / batch_size;
+
+    
     int dim = 7;
-    int t_steps = 256;
+    int t_steps = 80;
     float dt = 1.0f / t_steps;
 
     // Allocate xt
-    float* xt = (float*) malloc(n_samples * dim * sizeof(float));
-
-    // Initialize random xt ~ N(0,1)
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < n_samples * dim; i++) {
-        float u1 = ((float) rand()) / (float)RAND_MAX;
-        float u2 = ((float) rand()) / (float)RAND_MAX;
-        xt[i] = sqrtf(-2.0f * logf(u1)) * cosf(2.0f * M_PI * u2);
-    }
+    float* xt = (float*) malloc(batch_size * dim * sizeof(float));
+    float* save_arr = (float*) malloc(10*batch_size * dim * sizeof(float));
 
     // Create memory info (CPU)
     OrtMemoryInfo* memory_info;
@@ -101,132 +118,116 @@ int main() {
     const char* output_names[] = {"output"};
 
     // Shapes
-    int64_t xt_shape[] = {n_samples, dim};
-    int64_t t_shape[]  = {n_samples, 1};
+    int64_t xt_shape[] = {batch_size, dim};
+    int64_t t_shape[]  = {batch_size, 1};
 
     // Temp buffers
-    float* t_vals = (float*) malloc(n_samples * sizeof(float));
-    float* output   = (float*) malloc(n_samples * dim * sizeof(float));
+    float* t_vals = (float*) malloc(batch_size * sizeof(float));
+    float* output   = (float*) malloc(batch_size * dim * sizeof(float));
 
-    // ---------------------------
-    // Time integration loop
-    // ---------------------------
+    struct timespec tot_start, tot_end;
+    clock_gettime(CLOCK_MONOTONIC, &tot_start);
     // Create Ort tensors
     OrtValue* xt_tensor;
     OrtValue* t_tensor;
-    OrtValue* output_tensor = NULL;
-    printf("\nStarting time loop\n");
-    for (int step = 0; step < t_steps; step++) {
-      
-        struct timespec start, end;
-        clock_gettime(CLOCK_MONOTONIC, &start);
-
-
-        float t_scalar = (float) step / (float) t_steps;
-        printf("t=%g\n", t_scalar);
-
-        // Fill t batch
-        for (int i = 0; i < n_samples; i++) {
-            t_vals[i] = t_scalar;
-        }
-
-
-
-        CHECK(api->CreateTensorWithDataAsOrtValue(
-            memory_info, xt, n_samples * dim * sizeof(float),
-            xt_shape, 2, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &xt_tensor));
-        CHECK(api->CreateTensorWithDataAsOrtValue(
-            memory_info, t_vals, n_samples * sizeof(float),
-            t_shape, 2, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &t_tensor));
-
-        OrtValue* input_tensors[] = {xt_tensor, t_tensor};
-
-        // Run model
-        CHECK(api->Run(
-            session,
-            NULL,
-            input_names,
-            (const OrtValue* const*)input_tensors,
-            2,
-            output_names,
-            1,
-            &output_tensor
-        ));
-
-        // Get prediction data
-        float* output_data;
-        CHECK(api->GetTensorMutableData(output_tensor, (void**)&output_data));
-
-        // Euler step: xt += dt * output
-        for (int i = 0; i < n_samples * dim; i++) {
-            xt[i] += dt * output_data[i];
-        }
-
-        
-        clock_gettime(CLOCK_MONOTONIC, &end);
+    OrtValue* output_tensor;
     
-        double elapsed =
-            (end.tv_sec - start.tv_sec) +
-            (end.tv_nsec - start.tv_nsec) / 1e9;
-    
-        printf("Step %d took %.6f seconds\n", step, elapsed);
-  
+    CHECK(api->CreateTensorWithDataAsOrtValue(
+        memory_info, xt, batch_size * dim * sizeof(float),
+        xt_shape, 2, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &xt_tensor));
 
+    CHECK(api->CreateTensorWithDataAsOrtValue(
+        memory_info, t_vals, batch_size * sizeof(float),
+        t_shape, 2, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &t_tensor));
+
+    CHECK(api->CreateTensorWithDataAsOrtValue(
+    memory_info, output, batch_size * dim * sizeof(float),
+    xt_shape, 2, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &output_tensor));
+
+    OrtValue* input_tensors[] = {xt_tensor, t_tensor};
+
+    // ========================================================================
+    // ======================== START SAMPLING LOOP ===========================
+    // ========================================================================
+
+    printf("\nStarting time loop for %d batches in %d steps\n", n_batches, t_steps);
+    
+    // buffer to accumulate all samples
+    float* out_arr = (float*) malloc(batch_size*n_batches * dim * sizeof(float));
+    
+    for (int batch = 0; batch < n_batches; batch++) {
+        int offset = batch * batch_size;
+        // Reset + reinitialize xt ~ N(0,1) for this batch
+        for (int i = 0; i < batch_size * dim; i++) {
+            float u1 = ((float) rand()) / (float)RAND_MAX;
+            float u2 = ((float) rand()) / (float)RAND_MAX;
+            xt[i] = sqrtf(-2.0f * logf(u1)) * cosf(2.0f * M_PI * u2);
+        }
+    
+        // update tensor shapes if needed
+        int64_t xt_shape[] = {batch_size, dim};
+        int64_t t_shape[]  = {batch_size, 1};
+        for (int step = 0; step < t_steps; step++) {
+    
+            float t_scalar = (float) step / (float) t_steps;
+    
+            // fill t
+            #pragma omp simd
+            for (int i = 0; i < batch_size; i++) {
+                t_vals[i] = t_scalar;
+            }
+    
+            CHECK(api->Run(
+                session, NULL,
+                input_names,
+                (const OrtValue* const*)input_tensors, 2,
+                output_names, 1,
+                &output_tensor
+            ));
+    
+            float* output_data;
+            CHECK(api->GetTensorMutableData(output_tensor, (void**)&output_data));
+    
+            #pragma omp simd
+            for (int i = 0; i < batch_size * dim; i++) {
+                xt[i] += dt * output_data[i];
+            }
+        }
+    
+        // store batch into final array
+        memcpy(out_arr + offset * dim, xt, batch_size * dim * sizeof(float));
+    
+        // periodic save
+        if ((batch + 1) % 10 == 0 || batch == n_batches - 1) {
+            save_to_csv("C_samples.csv", out_arr, offset + batch_size, dim);
+            clock_gettime(CLOCK_MONOTONIC, &tot_end);
+            double elapsed =
+              (tot_end.tv_sec - tot_start.tv_sec) +
+              (tot_end.tv_nsec - tot_start.tv_nsec) / 1e9;
+            printf("\nBatch %d! Samples generated %d, Time taken %g seconds\n",batch, batch_size*(batch+1), elapsed );
+        }
     }
+    
+    // final cleanup
+    free(out_arr);
+    
     // Cleanup tensors
     api->ReleaseValue(xt_tensor);
     api->ReleaseValue(t_tensor);
     api->ReleaseValue(output_tensor);
-    // ---------------------------
-    // Print result
-    // ---------------------------
-    // printf("\n✅ Final samples:\n");
-    // for (int i = 0; i < n_samples; i++) {
-    //     printf("Sample %d: ", i);
-    //     for (int j = 0; j < dim; j++) {
-    //         printf("%f ", xt[i * dim + j]);
-    //     }
-    //     printf("\n");
-    // }
-    save_to_csv("C_samples.csv", xt, n_samples, dim);
+    
+    clock_gettime(CLOCK_MONOTONIC, &tot_end);
+    double elapsed =
+            (tot_end.tv_sec - tot_start.tv_sec) +
+            (tot_end.tv_nsec - tot_start.tv_nsec) / 1e9;
+    printf("\nSample generation over! Time taken %g seconds\n", elapsed );
+
 
     // Cleanup
     free(xt);
     free(t_vals);
     free(output);
     api->ReleaseMemoryInfo(memory_info);
-
-    // --- Optional: Inspect model inputs ---
-    // size_t num_input_nodes;
-    // CHECK(api->SessionGetInputCount(session, &num_input_nodes));
-    // printf("Number of inputs: %zu\n", num_input_nodes);
-    //
-    // for (size_t i = 0; i < num_input_nodes; i++) {
-    //     char* input_name;
-    //     OrtAllocator* allocator;
-    //     CHECK(api->GetAllocatorWithDefaultOptions(&allocator));
-    //
-    //     CHECK(api->SessionGetInputName(session, i, allocator, &input_name));
-    //     printf("Input %zu name: %s\n", i, input_name);
-    //
-    //     CHECK(api->AllocatorFree(allocator, input_name));
-    // }
-    //
-    // size_t num_output_nodes;
-    // CHECK(api->SessionGetOutputCount(session, &num_output_nodes));
-    // printf("Number of outputs: %zu\n", num_output_nodes);
-    //
-    // for (size_t i = 0; i < num_output_nodes; i++) {
-    //     char* output_name;
-    //     OrtAllocator* allocator;
-    //     CHECK(api->GetAllocatorWithDefaultOptions(&allocator));
-    //     CHECK(api->SessionGetOutputName(session, i, allocator, &output_name))
-    //     printf("Output %zu name: %s\n", i, output_name);
-    //
-    //     CHECK(api->AllocatorFree(allocator, output_name));
-    // }
-
-    // --- Cleanup ---
     api->ReleaseSession(session);
     api->ReleaseSessionOptions(session_options);
     api->ReleaseEnv(env);
