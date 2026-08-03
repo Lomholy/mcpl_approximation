cae97ba3bbdc2a616db75a28b4487e165c3def3b
Diablo <daniel.lomholt.2000@gmail.com>
2026-08-03 10:27:26 +0200

    Simplify script to always plot, and remove transformation back into ordinary neutron phase space

--- VAE/vae_correlations.py
+++ VAE/vae_correlations.py
@@ -6,7 +6,6 @@ import argparse
 import sys
 
 sys.path.append("..")
-from data_load import postprocess_nn, save_data_as_mcpl
 from plotting import plot_correlations_7d
 
 
@@ -20,13 +19,17 @@ def plot_losses(
 ):
     fig, ax = plt.subplots()
     train_losses = np.load(filename_train)
-    ax.plot(train_losses, label="Train loss")
-    ax.set(yscale="log")
+    val_losses = np.load(filename_val)
+    print(val_losses)
+    ax.plot(val_losses[:,0], train_losses, ".", label="Train loss")
+    ax.plot(val_losses[:,0], val_losses[:,1], ".", label="Validation loss")
+
     ax.set(
         title="Loss per iteration",
         xlabel="Iteration",
         ylabel="Loss [reconstruction + Kullback Liebler div]",
     )
+    ax.legend()
     return fig, ax
 
 
@@ -51,10 +54,6 @@ ckpt = torch.load(filename, map_location=device)
 vae = VAE().to(device)
 vae.load_state_dict(ckpt["state_dict"])
 vae.eval()
-lims = np.load("../limits.npy")
-mins = torch.asarray(lims[0])
-maxs = torch.asarray(lims[1])
-        
 with torch.no_grad():
     # Only include samples if they are within the limits of the original data
 
@@ -62,21 +61,15 @@ with torch.no_grad():
     samples = []
     while len(samples) < n_samples:
         print(len(samples))
-        # Limit samples to within the preprocessed data limits
-        z = torch.randn(10_000, vae.latent_dim).to(device)
+        z = torch.randn(10_000, vae.lat_dim).to(device)
         batch = torch.asarray(vae.decode(z).cpu())
-        batch = postprocess_nn(batch, filename="normalization_params.npy")
-        mask = (batch >= mins) & (batch <= maxs)
-        batch = batch[mask.all(axis=1)]
         samples = samples + batch.tolist()
     samples = samples[:n_samples]
     samples = torch.asarray(samples)
 
 
-if args.loss:
-    plot_losses()
-if args.plot:
-    plot_correlations_7d(samples, "Synthetic VAE: correlations postprocessed")
+plot_losses()
+plot_correlations_7d(samples, "Synthetic VAE: correlations postprocessed")
 
 # save_data_as_mcpl(samples, "../vae_samples")
 
