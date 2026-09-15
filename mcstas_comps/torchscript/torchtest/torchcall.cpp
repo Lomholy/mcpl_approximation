c46eea5f40e0db181136f378b09326590ac97043
Diablo <daniel.lomholt.2000@gmail.com>
2026-09-15 08:59:59 +0200

    Split main away from torchcall such that it can compile as a library

--- mcstas_comps/torchscript/torchtest/torchcall.cpp
+++ mcstas_comps/torchscript/torchtest/torchcall.cpp
@@ -1,5 +1,4 @@
-#include <torch/script.h>
-#include <torch/torch.h>
+#include "torchcall.h"
 
 #include <chrono>
 #include <iostream>
@@ -24,43 +23,4 @@ torch::Tensor run_model(
     return output_tuple->elements()[0].toTensor();
 }
 
-int main(int argc, const char* argv[])
-{
-    if (argc != 2) {
-        std::cerr << "usage: example-app <path-to-exported-script-module>\n";
-        return -1;
-    }
 
-    torch::jit::script::Module module;
-
-    try {
-        module = load_model(argv[1]);
-    } catch (const c10::Error& e) {
-        std::cerr << "error loading the model\n";
-        return -1;
-    }
-
-    std::cout << "Model loaded successfully\n";
-
-    auto device = torch::Device(torch::kMPS);
-    for (int i = 0; i<100; i++){
-        torch::Tensor input =
-            torch::randn({10000, 12}, torch::TensorOptions().device(device));
-
-        auto start = std::chrono::high_resolution_clock::now();
-        torch::Tensor output = run_model(module, input);
-        output.cpu();
-
-
-        // Ensure execution is complete before stopping timer
-        auto end = std::chrono::high_resolution_clock::now();
-
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                      end - start)
-                      .count();
-
-        std::cout << "Inference time: " << ms << " ms\n";
-    }
-
-    return 0;
-}
